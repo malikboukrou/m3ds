3330681f6ff72e69660572db75759ab03616f23e
whispyy <jfdurk@hotmail.fr>
2016-02-24 12:18:32 +0100

    jolie voiture roulante

--- tp3/carPlane_Durand_Boukrou/src/application/Car.cpp
+++ tp3/carPlane_Durand_Boukrou/src/application/Car.cpp
@@ -25,7 +25,7 @@ Car::Car() {
 void Car::drawRim() {
 
     p3d::modelviewMatrix.push();
-    p3d::diffuseColor=Vector3(0,1,50);
+    p3d::diffuseColor=Vector3(1,1,0);
     p3d::modelviewMatrix.scale(0.1,0.1,2);
     drawCylinder();
     p3d::modelviewMatrix.pop();
@@ -72,7 +72,7 @@ void Car::drawWheel() {
 
     p3d::modelviewMatrix.scale(2.5,2.5,2.5);
     p3d::modelviewMatrix.rotate(90,0,1,0);
-    p3d::diffuseColor=Vector3(0,1,80);
+    p3d::diffuseColor=Vector3(0,1,0);
     drawTorus();
 
     p3d::modelviewMatrix.pop();
@@ -80,8 +80,11 @@ void Car::drawWheel() {
 
 void Car::drawAxle() {
     p3d::modelviewMatrix.push();
+    p3d::modelviewMatrix.translate(-2.5,0,0);
+    p3d::modelviewMatrix.push();
     p3d::modelviewMatrix.rotate(90,0,1,0);
-    p3d::modelviewMatrix.scale(0.3,0.3,4.5);
+    p3d::modelviewMatrix.scale(0.3,0.3,5);
+    p3d::diffuseColor=Vector3(1,0,0);
     drawCylinder();
     p3d::modelviewMatrix.pop();
     p3d::modelviewMatrix.push();
@@ -91,29 +94,46 @@ void Car::drawAxle() {
     drawWheel();
     p3d::modelviewMatrix.pop();
     p3d::modelviewMatrix.push();
-    p3d::modelviewMatrix.translate(4.5,0,0);
+    p3d::modelviewMatrix.translate(5,0,0);
     drawRim();
     drawWheel();
+    p3d::modelviewMatrix.pop();
     p3d::modelviewMatrix.pop();
 }
 
 void Car::drawBody() {
-    p3d::modelviewMatrix.scale(3,1,1);
-    p3d::diffuseColor=Vector3(0,200,50);
+    p3d::modelviewMatrix.push();
+    p3d::modelviewMatrix.scale(6,2,2);
+    p3d::diffuseColor=Vector3(0,0.3,0.8);
+    p3d::modelviewMatrix.translate(-0.75,1.1,1.25);
     drawCube();
     p3d::modelviewMatrix.pop();
     p3d::modelviewMatrix.push();
-    p3d::modelviewMatrix.scale(1,1,1);
-    p3d::modelviewMatrix.translate(2,2,0);
+    p3d::modelviewMatrix.scale(2,2,2);
+    p3d::modelviewMatrix.translate(0.5,3.1,0);
+    p3d::modelviewMatrix.translate(-0.75,0,1.25);
     drawCube();
+    p3d::modelviewMatrix.pop();
 }
 
 void Car::draw() {
+  p3d::modelviewMatrix.scale(0.6,0.6,0.6);
+  p3d::modelviewMatrix.translate(0,-2.5,0);
   p3d::modelviewMatrix.push();
-
+  p3d::modelviewMatrix.translate(0,0,1);
+  p3d::modelviewMatrix.rotate(_rotateWheel,1,0,0);
   drawAxle();
-
-
+  p3d::modelviewMatrix.pop();
+  p3d::modelviewMatrix.push();
+  p3d::modelviewMatrix.translate(0,0,9.5);
+  p3d::modelviewMatrix.rotate(_steering,0,1,0);
+  p3d::modelviewMatrix.rotate(_rotateWheel,1,0,0);
+  drawAxle();
+  p3d::modelviewMatrix.pop();
+  p3d::modelviewMatrix.push();
+  p3d::modelviewMatrix.rotate(90,0,1,0);
+  p3d::modelviewMatrix.translate(0,0,-2.5);
+  drawBody();
   p3d::modelviewMatrix.pop();
 }
 
@@ -121,16 +141,23 @@ void Car::draw() {
 void Car::drawWorld() {
 
   p3d::modelviewMatrix.push();
-
+  p3d::modelviewMatrix.rotate(_orientation);
+  p3d::modelviewMatrix.translate(_position);
   draw(); // tracé de la voiture dans son repère local
   p3d::modelviewMatrix.pop();
 }
 
 void Car::move() {
+  const double speedLimit = 0.5;
   _acceleration+=-_velocity/50;
   _velocity+=_acceleration;
-  _rotateWheel+=_velocity*20;
+  _rotateWheel+=_velocity*10;
   _steering-=_steering/10*fabs(_velocity);
+
+  p3d::Vector3 _uneDirection;
+  _uneDirection.set(0, 0, _velocity);
+  _uneDirection *= speedLimit;
+  _position = _position+_uneDirection;
 
   _orientation.rotate(_steering*_velocity/(1.0+fabs(_velocity)),Vector3(0,1,0)); // le /(1.0+fabs(_velocity)) a été déterminé empiriquement
 
