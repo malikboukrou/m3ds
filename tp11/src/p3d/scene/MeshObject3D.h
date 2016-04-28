0e71926fbc69278dea8a3d0603010a6de8b5e3d9
malikboukrou <malik.boukrou@gmail.com>
2016-04-28 13:33:50 +0200

    tp11 added

---
+++ tp11/src/p3d/scene/MeshObject3D.h
@@ -0,0 +1,27 @@
+#ifndef MESHOBJECT3D_H
+#define MESHOBJECT3D_H
+
+#include "Mesh.h"
+#include "Object3D.h"
+
+
+/*!
+*
+* @file
+*
+* @brief
+* @author F. Aubert
+*
+*/
+
+namespace p3d {
+
+class MeshObject3D : public Mesh, public Object3D {
+public:
+  virtual ~MeshObject3D();
+  MeshObject3D();
+};
+}
+
+#endif // MESHOBJECT3D_H
+
